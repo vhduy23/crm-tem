c1b956dfb0fbbc12193164ff3252752ad3e630b9
vhduy23 <vhduy23@gmail.com>
2026-07-08 15:01:09 +0700

    fix text pdf 2

--- front/footer.php
+++ front/footer.php
@@ -147,11 +147,11 @@ renderCart();
             <img id="hdr-logo" src="<?php echo $logoBase64; ?>"
                 style="max-width:100%;max-height:100%;object-fit:contain;">
         </div>
-        <div style="background:#004B87;flex-grow:1;display:flex;align-items:center;padding:0 25px;box-sizing:border-box;">
-            <span style="font-family: Barlow, sans-serif;font-weight: bold;color:#fff;font-size:30px;font-weight:bold;text-transform:uppercase; display:inline-block; transform:translateY(-10px);">
-                GRAPHIC DESIGNS
-            </span>
-        </div>`;
+    <div style="background:#004B87;flex-grow:1;display:flex;justify-content:center;align-items:center;padding:0 25px;box-sizing:border-box;">
+        <span style="font-family:Barlow,sans-serif;font-size:35px;font-weight:bold;color:#fff;text-transform:uppercase; transform:translateY(-17px);">
+            GRAPHIC DESIGNS
+        </span>
+    </div>`;
     headerEl.style.position = 'fixed';
     headerEl.style.left = '-9999px';
     document.body.appendChild(headerEl);
